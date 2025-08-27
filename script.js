fdbe77b588754a62edb54d9603143ff5f6126e0f
Yogesh Summan <yogeshsumman2001@gmail.com>
2025-08-27 14:33:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+    let div = createElement('div');
+	div.textContent += "DOM load success"
+	document.body.appendChild(div)
+});
